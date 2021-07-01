54c872711ee3ce888a85d517af9020f4d0c67b0a
Ronald Arias <ariasron@hotmail.com>
2021-06-30 22:05:03 -0500

    Update .lintstagedrc.cjs

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -2,5 +2,5 @@ module.exports = {
 	'*.js': ['prettier --write', 'eslint --fix'],
 	'*.cjs': ['prettier --write', 'eslint --fix'],
 	'*.ts': ['prettier --write', 'eslint --fix'],
-	'*.svelte': ['prettier --write', 'eslint --fix', 'stylelint --fix']
+	'*.svelte': ['prettier --write', 'eslint --fix']
 };
